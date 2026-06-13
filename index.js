be15773959342067e9c2319cd8594acf2e1367f8
Aymene khoukhi <khoukhiaymen4@gmail.com>
2026-06-13 08:54:53 +0100

    Fix PORT to use Render default

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import adminRoutes from './routes/adminRouter.js';
 import storesRoutes from './routes/storesRouter.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // ── Middleware ──────────────────────────────────────────────────────────────
 app.use(cors({
