91e68d7ff566725d6bf1d1c1abdbb91363b14080
Aymene khoukhi <khoukhiaymen4@gmail.com>
2026-06-13 10:23:37 +0100

    Fix collation utf8mb4_unicode_ci in DB pool

--- db.js
+++ db.js
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   charset: 'utf8mb4',
+  collation: 'utf8mb4_unicode_ci',
 });
 
 // Verify tables exist on startup
